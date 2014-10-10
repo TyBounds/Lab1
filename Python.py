2024c2561a997c2ab4e09b20d036f909d59c0944
TyBounds <tybounds@live.com>
2014-10-10 14:43:57 -0500

    Lab1
    First lab

---
+++ Python.py
@@ -0,0 +1,17 @@
+print "Welcome"
+print "Enter 2 and 4 one at a time please"
+var1 = "2"
+var2 = "4"
+var1 = raw_input()
+print var1
+var2 = raw_input()
+print var2
+print "What is your name"
+Name = raw_input()
+print Name
+print "would you like to add or subtract"
+var1 = int(var1)
+var2 = int(var2)
+var3 = var1+var2
+
+print var3
