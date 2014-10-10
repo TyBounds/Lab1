644a69e980ad2d139e0969c950ee123494c67404
TyBounds <tybounds@live.com>
2014-10-10 14:46:50 -0500

    Lab4
    Lab4

---
+++ lab4.py
@@ -0,0 +1,12 @@
+def getFirstInList(list1):
+    print list1[0]
+getFirstInList([4,6,3,3,5,76,7,4,3,8])
+
+
+def getLastInList(list2):
+    length = len(list2)
+    lastPos = length - 1
+    print list2[lastPos]
+getLastInList([4,6,3,3,5,76,7,4,3,100])
+
+
